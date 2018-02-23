19093fda4773372e668ae7b8ec2fd0a4bb0c6852
Roma Sokolov <sokolov.r.v@gmail.com>
2018-02-23 18:00:20 +0100

    add dev profile

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject ptarmigan "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "delay"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot ptarmigan.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {
+             :uberjar {:aot :all}
+             :dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
+                   :repl-options {:init-ns user}
+                   }
+             })

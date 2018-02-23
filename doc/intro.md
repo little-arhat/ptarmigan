433f5612ca443422330b848f606ccb13493c7b78
Roma Sokolov <sokolov.r.v@gmail.com>
2018-02-23 15:22:25 +0100

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ptarmigan
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ptarmigan/core_test.clj
@@ -0,0 +1,7 @@
+(ns ptarmigan.core-test
+  (:require [clojure.test :refer :all]
+            [ptarmigan.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
